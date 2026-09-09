7ac4ffeb53d61176e293258fc3cfa8debde7efb9
sabriakadim800-blip <sabriakadim800@gmail.com>
2026-09-09 04:38:28 +0300

    Create TextureRenderer.kt

---
+++ app/src/main/java/com/skteam/subtitleburner/TextureRenderer.kt
@@ -0,0 +1,127 @@
+
+package com.skteam.subtitleburner
+
+import android.graphics.Bitmap
+import android.opengl.GLES20
+import android.opengl.GLUtils
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
+
+object TextureRenderer {
+
+    private const val VERTEX_SHADER = """
+        attribute vec4 aPosition;
+        attribute vec2 aTexCoord;
+        varying vec2 vTexCoord;
+        void main() {
+            vTexCoord = aTexCoord;
+            gl_Position = aPosition;
+        }
+    """
+
+    private const val FRAGMENT_SHADER = """
+        precision mediump float;
+        varying vec2 vTexCoord;
+        uniform sampler2D sTexture;
+        void main() {
+            gl_FragColor = texture2D(sTexture, vTexCoord);
+        }
+    """
+
+    private val FULL_RECT_COORDS = floatArrayOf(
+        -1f, -1f, 0f, 0f,
+         1f, -1f, 1f, 0f,
+        -1f,  1f, 0f, 1f,
+         1f,  1f, 1f, 1f
+    )
+
+    private var program = 0
+    private var aPositionLoc = 0
+    private var aTexCoordLoc = 0
+    private var sTextureLoc = 0
+    private var vertexBuffer: FloatBuffer
+
+    init {
+        vertexBuffer = ByteBuffer.allocateDirect(FULL_RECT_COORDS.size * 4)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer()
+        vertexBuffer.put(FULL_RECT_COORDS).position(0)
+    }
+    
+    fun init() {
+        program = createProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+        aPositionLoc = GLES20.glGetAttribLocation(program, "aPosition")
+        aTexCoordLoc = GLES20.glGetAttribLocation(program, "aTexCoord")
+        sTextureLoc = GLES20.glGetUniformLocation(program, "sTexture")
+    }
+
+    fun drawVideoTexture(texId: Int) {
+        drawTexture(texId, false)
+    }
+
+    fun drawOverlayTexture(texId: Int) {
+        drawTexture(texId, true)
+    }
+
+    private fun drawTexture(texId: Int, blend: Boolean) {
+        GLES20.glUseProgram(program)
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texId)
+        GLES20.glUniform1i(sTextureLoc, 0)
+
+        if (blend) {
+            GLES20.glEnable(GLES20.GL_BLEND)
+            GLES20.glBlendFunc(GLES20.GL_ONE, GLES20.GL_ONE_MINUS_SRC_ALPHA)
+        }
+
+        vertexBuffer.position(0)
+        GLES20.glEnableVertexAttribArray(aPositionLoc)
+        GLES20.glVertexAttribPointer(aPositionLoc, 2, GLES20.GL_FLOAT, false, 16, vertexBuffer)
+
+        vertexBuffer.position(2)
+        GLES20.glEnableVertexAttribArray(aTexCoordLoc)
+        GLES20.glVertexAttribPointer(aTexCoordLoc, 2, GLES20.GL_FLOAT, false, 16, vertexBuffer)
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
+
+        GLES20.glDisableVertexAttribArray(aPositionLoc)
+        GLES20.glDisableVertexAttribArray(aTexCoordLoc)
+
+        if (blend) GLES20.glDisable(GLES20.GL_BLEND)
+    }
+    
+    fun createTextureObject(): Int {
+        val textures = IntArray(1)
+        GLES20.glGenTextures(1, textures, 0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0])
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR.toFloat())
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR.toFloat())
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE)
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE)
+        return textures[0]
+    }
+
+    fun bitmapToTexture(bitmap: Bitmap?): Int {
+        if (bitmap == null) return 0
+        val texId = createTextureObject()
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
+        return texId
+    }
+
+    private fun createProgram(vertexSrc: String, fragmentSrc: String): Int {
+        val vs = loadShader(GLES20.GL_VERTEX_SHADER, vertexSrc)
+        val fs = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSrc)
+        val program = GLES20.glCreateProgram()
+        GLES20.glAttachShader(program, vs)
+        GLES20.glAttachShader(program, fs)
+        GLES20.glLinkProgram(program)
+        return program
+    }
+
+    private fun loadShader(type: Int, src: String): Int {
+        val shader = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(shader, src)
+        GLES20.glCompileShader(shader)
+        return shader
+    }
+}
